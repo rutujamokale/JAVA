ad1dd9a6da76dff9638fa8aa9f8bce9c8d996e91
Rutuja Mokale <rutujamokale2003@gmail.com>
2025-07-06 22:19:29 +0530

    Delete Simple_java_codes/Type.java

--- Simple_java_codes/Type.java
+++
@@ -1,9 +0,0 @@
-public class Type {
-    public static void main(String[] args) {
-        // concatenate
-        String name1 = "Rutu";
-        String name2 = "Mokale";
-        String name3 = name1 + name2;
-        System.out.println(name3);  // This will print: RutuMokale
-    }
-}
