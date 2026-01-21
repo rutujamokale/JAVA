7a211754b493312215d57aa26750256b92e3c7ac
rutujamokale <rutujamokale2003@gmail.com>
2026-01-21 23:06:11 +0530

    array examples

--- simple_java/src/main/java/com/transflower/App.java
+++ simple_java/src/main/java/com/transflower/App.java
@@ -1,9 +1,4 @@
 package com.transflower;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import com.transflower.Entity.Question;
 /**
  * Hello world!
  *
@@ -12,38 +7,30 @@ public class App
 {
     public static void main( String[] args )
     {
-        FileIoManager  manager = new FileIoManager();
 
-        List<Question> questions=new ArrayList<>();
-        questions.add(new Question(
-    "What is Java?",
-    "A programming language",  // optionA
-    "", "", "",                // optionB, optionC, optionD
-    'A',                      // correctAnswer
-    0,                        // evaluationCriteria
-    1,                        // id
-    101                       // subjectId
-));
+        int[] arr = { 10, 20, 30, 40 };
+        // int Max = arr[0];
 
+        // for (int i = 1; i < arr.length; i++) {
+        //     if (arr[i] > Max) {
+        //         Max = arr[i];
+        //     }
+        // }
 
-        manager.SaveToFile(questions);
-
-        List<Question>loaded=manager.loadFromFile();
-        System.out.println("loaded:" +loaded);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        int Min = arr[0];
+        for (int x : arr) {
+            if (x < Min)
+                Min = x;
+        }
+        
+        System.out.println("min value" + Min);
+        
+        
+        
+        
+        
+        
+        
         // Circle c=new Circle();
         // Rectangle r=new Rectangle();
 
@@ -54,9 +41,9 @@ public class App
         // r.show();
 
         // Student s1=new Student();
-      
+
         //    //setter
-           
+
         //    s1.setName("rutuja");
         //    s1.setRoll_no(28);
         //    s1.setmarks(90);
@@ -65,12 +52,9 @@ public class App
         //    System.out.println("roll_no:" +s1.getRoll_no());
         //    System.out.println("marks:" + s1.getMarks());
 
-
         // Student s1=new Student("nikita",29,90.5f);
-        
-        // System.out.println(s1);
 
-        
+        // System.out.println(s1);
 
     }
 }
--- simple_java/src/main/java/com/transflower/Hello.java
+++ simple_java/src/main/java/com/transflower/Hello.java
@@ -3,6 +3,56 @@ package com.transflower;
 public class Hello{
     public static void main(String [] args){
         System.out.println("hi i am rutuja");
+
+        // int arr[] = { 10, 20, 30, 40 };
+
+
+        // int sum = 0;
+        // for (int x : arr) {
+        //     sum += x;
+        // }
+
+        // double average = sum / arr.length;
+       
+        // System.out.println("sum" + sum);
+
+        // System.out.println("average" + average);
+
+        // for (int i = arr.length - 1; i >= 0;i --){
+        //         System.out.println( arr[i] +"");
+        // }
+
+        // int Max = 0;
+        // for (int i = 0; i < arr.length; i++) {
+        //     if (arr[i] > Max)
+        //         Max = arr[i];
+        // }
+        
+        // System.out.println("max value " + Max);
+
+
+
+        // int Min = arr[0];
+
+        // for (int x : arr) {
+        //     if (x < Min)
+        //         Min = x;
+        // }
+
+        // System.out.println("min value" + Min);
+
+        int[] arr = { 1, 2, 3, 2, 4, 1 };
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j< arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    System.out.println("Duplicate" + arr[i]);
+                    break;
+                }
+            }
+        }
+
+        
     }
 }
 
