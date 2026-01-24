a4394d590905977feea28c0346ca305123e61364
rutujamokale <rutujamokale2003@gmail.com>
2026-01-24 22:34:43 +0530

    adding interface

--- my-app/src/main/java/com/HR/SalesManager.java
+++ my-app/src/main/java/com/HR/SalesManager.java
@@ -1,6 +1,12 @@
 package com.hr;
 
-public class SalesManager extends SalesEmployee  {
+import com.hr.Interface.IInterviewPanel;
+import com.hr.Interface.IManagerBenefits;
+import com.hr.Interface.ITrainer;
+
+public class SalesManager extends SalesEmployee implements  IManagerBenefits,
+                            IInterviewPanel,
+                            ITrainer  {
     int bonus;
 
      public SalesManager(int id, String firstName, String lastName, String email, long contactNumber, String location,
@@ -21,4 +27,34 @@ public class SalesManager extends SalesEmployee  {
      public int computePay() {
          return super.computePay() + this.bonus;
      }
+
+     @Override
+
+     public float CalculateBonus() {
+         return bonus;
+     }
+     
+     @Override
+     public void ConductAppraisal()
+     {
+         System.out.println("Manager appraisal completed.");
+     }
+
+     @Override
+    public void ApproveLeave()
+    {
+        System.out.println("Leave approved by Sales Manager.");
+    }
+
+      @Override
+    public void TakeInterview()
+    {
+        System.out.println("Sales Manager conducting interview.");
+    }
+
+    @Override
+    public void Train()
+    {
+        System.out.println("Sales Manager training sales team.");
+    }
 }
