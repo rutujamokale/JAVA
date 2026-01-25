db1fecd4851fe3e5728aa09f7bd3c85e2c83788d
rutujamokale <rutujamokale2003@gmail.com>
2026-01-25 10:54:02 +0530

    HR system

--- my-app/src/main/java/com/HR/App/App.java
+++ my-app/src/main/java/com/HR/App/App.java
@@ -1,7 +1,12 @@
 package com.hr.App;
 
 import com.hr.Employee;
+import com.hr.Interface.IAppraisable;
+import com.hr.Interface.IBonusEligible;
+import com.hr.Interface.IInterviewPanel;
+import com.hr.Interface.ITrainer;
 import com.hr.SalesEmployee;
+import com.hr.SalesManager;
 
 /**
  * Hello world!
@@ -21,7 +26,28 @@ public class App {
          emp2.doWork();
          
          System.out.println(emp1.computePay());
-         System.out.println(emp2.computePay());         
+         System.out.println(emp2.computePay());
+         
+         
+         SalesManager manager = new SalesManager(1, "rutuja", "mokale", "rutujamokale2003@gmail.com", 9529259355L,
+                         "mumbai", "22-06-2003", 10000, 5000, 20000, 40000, 2000, 20000);
+
+         IAppraisable appraisable = manager;
+                 
+         appraisable.ConductAppraisal();
+
+
+         IBonusEligible bonusEligible = manager;
+
+         System.out.println("bonus:" + bonusEligible.CalculateBonus());
+
+
+         IInterviewPanel panel = manager;
+         panel.TakeInterview();
+
+
+         ITrainer trainer = manager;
+              trainer.Train();
 
         
     }
