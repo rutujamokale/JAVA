1ce55b06f9eb7de0ce412c53bdfaa358d7c9e018
rutujamokale <rutujamokale2003@gmail.com>
2026-02-17 12:33:30 +0530

    add hello servlet

--- ServletDemo/src/main/java/com/demo/servlet/HelloServlet.java
+++ ServletDemo/src/main/java/com/demo/servlet/HelloServlet.java
@@ -1,5 +1,15 @@
 package com.demo.servlet;
 
-public class HelloServlet {
-    
+import java.io.IOException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        res.setContentType("text/html");
+        res.getWriter().println("<h1>Hello from maven servlet ! </h1>");
+    }
 }
