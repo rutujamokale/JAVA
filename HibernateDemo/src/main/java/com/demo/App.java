423ec78baa13d3d34e014e046964041d236a5789
rutujamokale <rutujamokale2003@gmail.com>
2026-02-20 12:06:23 +0530

    hibernate project using crud operation

---
+++ HibernateDemo/src/main/java/com/demo/App.java
@@ -0,0 +1,37 @@
+package com.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Configuration cfg = new Configuration();
+        cfg.configure("hibernate.cfg.xml");
+
+        SessionFactory factory = cfg.buildSessionFactory();
+        Session session = factory.openSession();
+
+        Transaction tx = session.beginTransaction();
+
+         Student s = new Student();
+         s.setId(4);
+         s.setName("reena");
+         s.setMarks(80);
+
+         session.save(s);
+
+        // Student s = session.get(Student.class, 4);
+        // if (s != null) {
+        //     s.setMarks(90);
+        //     session.update(s);
+        // }
+        tx.commit();
+
+        session.close();
+        factory.close();
+    }
+}
