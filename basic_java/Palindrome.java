6f01413bde67b1c00ad5eb74d956842b01d9e3af
rutujamokale <rutujamokale2003@gmail.com>
2026-02-24 22:33:33 +0530

    palindrome

---
+++ basic_java/Palindrome.java
@@ -0,0 +1,9 @@
+public class Palindrome {
+    public static void main(String[] args) {
+        String str = "madam";
+        String rev = new StringBuilder(str).reverse().toString();
+        System.out.println("reversed string: " +rev );
+
+
+    }
+}
---
+++ basic_java/tempCodeRunnerFile.java
@@ -0,0 +1,13 @@
+public class Reverse{
+    public static void main(String[] args) {
+        String st = "hello";
+        String res = "";
+
+        for (int i = st.length() - 1; i >= 0; i--) {
+            res += st.charAt(i);
+        }
+
+        System.out.println("reverse string" + res);
+
+    }
+}
