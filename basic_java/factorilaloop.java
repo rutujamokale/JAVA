b390af3d21ac8ea765511669a846934ed02800b5
rutujamokale <rutujamokale2003@gmail.com>
2026-02-24 23:37:07 +0530

    factorial

---
+++ basic_java/factorilaloop.java
@@ -0,0 +1,12 @@
+public class factorilaloop {
+    public static void main(String[] args) {
+        int num = 5;
+        int factorial = 1;
+
+        for (int i = 1; i <= num; i++) {
+            factorial = factorial * i;
+            ;
+        }
+        System.out.println("factorila:" + factorial);
+    }
+}
