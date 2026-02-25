fd6c0b4b67cc260756f2e1ec54ecf0f919c2e615
rutujamokale <rutujamokale2003@gmail.com>
2026-02-25 19:00:13 +0530

    prime number

---
+++ basic_java/primenum.java
@@ -0,0 +1,23 @@
+public class primenum {
+    public static void main(String[] args) {
+        int num = 17;
+        boolean isPrime = true;
+
+        if (num <= 1) {
+            isPrime = false;
+        }
+
+        for (int i = 2; i <= num / 2; i++) {
+            if (num % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        if (isPrime) {
+            System.out.println("prime number");
+
+        } else {
+            System.out.println("Not prime number");
+        }
+    }
+}
