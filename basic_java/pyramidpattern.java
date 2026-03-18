555a26fe49962e16b0af9a858e0766c3c1a51333
rutujamokale <rutujamokale2003@gmail.com>
2026-03-18 23:12:55 +0530

    pyramid pattern

---
+++ basic_java/pyramidpattern.java
@@ -0,0 +1,13 @@
+public class pyramidpattern {
+    
+    public static void main(String[] args) {
+        int n=5;
+        for (int i = 1; i <= 5;i++) {
+            
+
+            for (int j = 1; j <= n - 1; j++) {
+                System.out.print(" ");
+            }
+        }
+    }
+}
