6adf17b0f2395f0983b11afd06075d0995804a3c
rutujamokale <rutujamokale2003@gmail.com>
2026-03-19 23:12:48 +0530

    diamond problem

--- basic_java/diamond.java
+++ basic_java/diamond.java
@@ -1,5 +1,26 @@
 public class diamond {
     public static void main(String[] args) {
         int n = 5;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= (2 * n - 1); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        
+
+
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= (2 * n - 1); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
