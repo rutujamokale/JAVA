e08a3fd4690a69f202956c27e0d3c1d27af33d1b
rutujamokale <rutujamokale2003@gmail.com>
2026-03-22 17:18:15 +0530

    reverse number

---
+++ basic_java/reverseno.java
@@ -0,0 +1,20 @@
+public class reverseno {
+    public static void main(String[] args) {
+        int num = 121;
+        int original = num;
+        int rev = 0;
+
+        while (num > 0) {
+            int rem = num % 10;
+            rev = rev * 10 + rem;
+            num = num / 10;
+        }
+        if (original == rev) {
+            System.out.println("this is palindrome");
+       }else
+       {
+           System.out.println("this is not palindrome ");
+       }
+
+    }
+}
