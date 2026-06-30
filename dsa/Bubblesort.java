8518b8540a9b5c49acc92903adf20aa0f514b611
rutujamokale <rutujamokale2003@gmail.com>
2026-06-30 16:04:24 +0530

    bubble sort

--- dsa/Bubblesort.java
+++ dsa/Bubblesort.java
@@ -2,5 +2,23 @@
 public class Bubblesort{
     public  static void sort(int [] arr){
     int n= arr.length
+
+    for(int i=0;i<n-1;i++){
+        for(j=0;j<n-1-i;j++){
+            if(arr[j]>arr[j+1]){
+                temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
+    }
+    }
+
+    public static void main(String []args){
+         int []arr= {5,2,10,3};
+
+         for(int num:arr){
+              S  
+         }  
     }
 }
